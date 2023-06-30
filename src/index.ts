0b1ca400f2fc98881ac132ce09d810c94da1e177
dairinka <interest4me@gmail.com>
2023-06-30 22:50:11 +0300

    feat: add server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello ts');
+import http, { IncomingMessage, ServerResponse } from 'http';
+import 'dotenv/config';
+import { serverAnswer } from './serverAnswer';
+import { HttpMethod } from './types/types';
+
+const PORT = Number(process.env.PORT);
+
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  console.log('Server request');
+  console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'application/json');
+
+  const method = req.method as HttpMethod;
+  const data = JSON.stringify(serverAnswer({ url: req.url, method: method }));
+  res.end(data);
+});
+
+server.on('error', (err) => {
+  console.log(err.message);
+});
+
+server.listen(PORT, () => {
+  console.log(`listening port ${PORT}`);
+});
