857c2007ba6a20d99d1c85030dc7ef17d3c6063a
dairinka <interest4me@gmail.com>
2023-07-01 14:35:17 +0300

    feat: add data from user and methods

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import 'dotenv/config';
 import { serverAnswer } from './serverAnswer';
-import { HttpMethod } from './types/types';
+import { HttpMethod, IServerAnswer } from './types/types';
 
 const PORT = Number(process.env.PORT);
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
   console.log('Server request');
   console.log(req.url, req.method);
+  let dataFromUser = '';
+  req.on('data', (chunk) => {
+    dataFromUser += chunk;
+  });
+  req.on('end', () => {
+    res.setHeader('Content-Type', 'application/json');
+    const method = req.method as HttpMethod;
+    const answer: IServerAnswer = serverAnswer({
+      url: req.url,
+      method: method,
+      data: dataFromUser,
+    });
+    const dataAnswer = JSON.stringify(answer.resolve) || '';
+    res.statusCode = answer.status.statusCode || 200;
+    res.statusMessage = answer.status?.message;
 
-  res.setHeader('Content-Type', 'application/json');
-
-  const method = req.method as HttpMethod;
-  const data = JSON.stringify(serverAnswer({ url: req.url, method: method }));
-  res.end(data);
+    res.end(dataAnswer);
+  });
 });
 
 server.on('error', (err) => {
--- src/serverAnswer.ts
+++ src/serverAnswer.ts
@@ -1,32 +1,49 @@
 import { v4 as uuidv4 } from 'uuid';
-import { IServerAnswer, HttpMethod, IServerReject, IServerResolve } from './types/types';
+import { IServerAnswer, HttpMethod, IServerResolve } from './types/types';
+import { currentUserDb } from './database/UsersDb';
 
 interface IServerGet {
   url: string;
   method: HttpMethod;
+  data?: string;
 }
+
 const fakeData: IServerResolve = {
   id: uuidv4(), //— unique identifier (string, uuid) generated on server side
   username: 'Petro', //— user's name (string, required)
   age: 24, //— user's age (number, required)
   hobbies: ['skating'], //— user's hobbies (array of strings or empty array, required)
-  statusCode: 200,
 };
 
-const serverAnswer = ({ url, method }: IServerGet): IServerAnswer => {
-  console.log(url, method);
+const serverAnswer = (dataToServer: IServerGet): IServerAnswer => {
+  console.log(dataToServer.url, dataToServer.method);
   const endpoint = '/api/users';
-  switch (url) {
-    case '/api/users': {
-      if (method === 'GET') {
-        return { resolve: fakeData };
-      }
-      break;
+  if (dataToServer.url.match(/^\/api\/users./)) {
+    const db = currentUserDb;
+    switch (dataToServer.method) {
+      case 'PUT':
+        db.addUser();
+        break;
+      case 'GET':
+        db.getUsers();
+        break;
+      case 'POST':
+        break;
+      case 'DELETE':
+        db.deleteUserById();
+        break;
+      default:
+        return;
     }
-    default: {
-      return { reject: { statusCode: 404, message: 'such url not found' } };
-    }
+    // case '/api/users': {
+    //   if (method === 'GET') {
+    //     return { resolve: fakeData };
+    //   }
+    //   break;
+  } else {
+    return { status: { statusCode: 404, message: 'such url not found' } };
   }
+
   return {};
 };
 
